4dd5be6fd51f96cf06a60fab6333e72926afab76
Sven Petroll <sven.petroll@googlemail.com>
2010-02-19 00:17:09 +0100

    Muh3

---
+++ VoronoiEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TronBot
+{
+    public class VoronoiEvaluator : Evaluator
+    {
+        #region Evaluator Member
+
+        public int evaluation(GameState g, int player)
+        {
+            MapAnalyzer ma = new MapAnalyzer(g.Map);
+            int[] voronoi = MapManipulator.voronoiTerritory(g.Map, player == 0 ? g.Opponent : g.Player, player == 1 ? g.Opponent : g.Player);
+            
+            return voronoi[1] - voronoi[0];
+        }
+
+        #endregion
+    }
+}
